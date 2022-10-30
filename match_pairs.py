8e6cb5523caccf29a5d9151e1bd6b572ee15501a
Anton Bushuiev <anton.bushuiev@gmail.com>
2022-10-30 16:14:28 +0100

    Fix bug with name collisions

--- match_pairs.py
+++ match_pairs.py
@@ -63,6 +63,15 @@ from models.utils import (compute_pose_error, compute_epipolar_error,
 torch.set_grad_enabled(False)
 
 
+def pair_names_to_id(names):
+    # Remove extention
+    names = [str(Path(name).with_suffix('')) for name in names]
+    # Replace '/'
+    names = [name.replace('/', '__') for name in names]
+    # Concat
+    return names[0] + '___' + names[1]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Image pair matching and pose evaluation with SuperGlue',
@@ -217,11 +226,11 @@ if __name__ == '__main__':
     for i, pair in enumerate(pairs):
         name0, name1 = pair[:2]
         stem0, stem1 = Path(name0).stem, Path(name1).stem
-        matches_path = output_dir / '{}_{}_matches.npz'.format(stem0, stem1)
-        eval_path = output_dir / '{}_{}_evaluation.npz'.format(stem0, stem1)
-        viz_path = output_dir / '{}_{}_matches.{}'.format(stem0, stem1, opt.viz_extension)
-        viz_eval_path = output_dir / \
-            '{}_{}_evaluation.{}'.format(stem0, stem1, opt.viz_extension)
+        pair_id = pair_names_to_id((name0, name1))
+        matches_path = output_dir / f'{pair_id}_matches.npz'
+        eval_path = output_dir / f'{pair_id}_evaluation.npz'
+        viz_path = output_dir / f'{pair_id}_matches.{opt.viz_extension}'
+        viz_eval_path = output_dir / f'{pair_id}_evaluation.{opt.viz_extension}'
 
         # Handle --cache logic.
         do_match = True
@@ -280,7 +289,7 @@ if __name__ == '__main__':
         # Load the optional custom points
         if opt.input_points is not None:
             input_points_dir = Path(opt.input_points)
-            with open(input_points_dir / f'{stem0}_{stem1}.pkl', 'rb') as f:
+            with open(input_points_dir / f'{pair_id}.pkl', 'rb') as f:
                 pts0, pts1 = pickle.load(f)
         else:
             pts0, pts1 = None, None
@@ -296,11 +305,13 @@ if __name__ == '__main__':
             pred = {k: v[0].cpu().numpy() for k, v in pred.items()}
             kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
             matches, conf = pred['matches0'], pred['matching_scores0']
+            matches1, conf1 = pred['matches1'], pred['matching_scores1']
             timer.update('matcher')
 
             # Write the matches to disk.
             out_matches = {'keypoints0': kpts0, 'keypoints1': kpts1,
-                           'matches': matches, 'match_confidence': conf}
+                           'matches0': matches, 'match_confidence0': conf,
+                           'matches1': matches1, 'match_confidence1': conf1}
             np.savez(str(matches_path), **out_matches)
 
         # Keep the matching keypoints.
